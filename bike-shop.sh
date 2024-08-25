5c8a84479f01b3ffb210fdbfb000c928492e44c5
Igor Anzheyevskiy Zakharyk <andzheyevskiy@outlook.com>
2024-08-25 14:47:32 +0000

    Save progress: 1000.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -24,11 +24,14 @@ MAIN_MENU() {
 
 RENT_MENU() {
   #get available bikes
-AVAILABLE_BIKES=$($PSQL "SELECT bike_id,type,size FROM bikes WHERE available=false ORDER BY bike_id")
+AVAILABLE_BIKES=$($PSQL "SELECT bike_id,type,size FROM bikes WHERE available=true ORDER BY bike_id")
 echo "$AVAILABLE_BIKES"
   #if no bikes available
+if [[ -z $AVAILABLE_BIKES ]]; then
 
   #send to main menu
+
+fi
 }
 
 RETURN_MENU() {
