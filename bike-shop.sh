62316205c95d5d756c4a98a78d53c9d4be852252
Igor Anzheyevskiy Zakharyk <andzheyevskiy@outlook.com>
2024-08-25 14:39:28 +0000

    Save progress: 760.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -3,34 +3,32 @@
 echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
 
 MAIN_MENU() {
-
-  if [[ $1 ]]; then
+  if [[ $1 ]]
+  then
     echo -e "\n$1"
   fi
-  echo "How may I help you?\n"
-  echo -e "\n1. Rent a bike"
-  echo -e "2. Return a bike"
-  echo -e "3. Exit"
 
+  echo "How may I help you?" 
+  echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
   read MAIN_MENU_SELECTION
 
-  case $MAIN_MENU_SELECTION in 
-    1) RENT_MENU;;
-    2) RETURN_MENU;;
-    3) EXIT;;
-    *) MAIN_MENU "Please enter a valid option.";;
+  case $MAIN_MENU_SELECTION in
+    1) RENT_MENU ;;
+    2) RETURN_MENU ;;
+    3) EXIT ;;
+    *) MAIN_MENU "Please enter a valid option." ;;
   esac
-
 }
-RENT_MENU(){
+
+RENT_MENU() {
   echo "Rent Menu"
 }
 
-RETURN_MENU(){
+RETURN_MENU() {
   echo "Return Menu"
 }
 
-EXIT(){
+EXIT() {
   echo -e "\nThank you for stopping in.\n"
 }
 
