bc0d92f807e39086849a4de813cca2434343e0be
Igor Anzheyevskiy Zakharyk <andzheyevskiy@outlook.com>
2024-08-25 15:40:52 +0000

    Save progress: 1417.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -65,16 +65,16 @@ if [[ -z $AVAILABLE_BIKES ]]; then
       INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name,phone) VALUES('$CUSTOMER_NAME','$PHONE_NUMBER')")
       fi
     # get customer_id
-CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$PHONE_NUMBER'")
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$PHONE_NUMBER'")
     # insert bike rental
-INSERT_RENTAL_RESULT=$($PSQL "INSERT INTO rentals(customer_id,bike_id) VALUES($CUSTOMER_ID,$BIKE_ID_TO_RENT)")
+    INSERT_RENTAL_RESULT=$($PSQL "INSERT INTO rentals(customer_id,bike_id) VALUES($CUSTOMER_ID,$BIKE_ID_TO_RENT)")
     # set bike availability to false
-SET_TO_FALSE_RESULT=$($PSQL "UPDATE bikes SET available=false WHERE bike_id=$BIKE_ID_TO_RENT")
+    SET_TO_FALSE_RESULT=$($PSQL "UPDATE bikes SET available=false WHERE bike_id=$BIKE_ID_TO_RENT")
     # get bike info
-BIKE_INFO=$($PSQL "SELECT size, type FROM bikes WHERE bike_id=$BIKE_ID_TO_RENT")
-BIKE_INFO_FORMATTED=$(echo $BIKE_INFO | sed 's/ |/"/')
+    BIKE_INFO=$($PSQL "SELECT size, type FROM bikes WHERE bike_id=$BIKE_ID_TO_RENT")
+    BIKE_INFO_FORMATTED=$(echo $BIKE_INFO | sed 's/ |/"/')
     # send to main menu
-
+    MAIN_MENU "I have put you down for the $BIKE_INFO_FORMATTED Bike, $CUSTOMER_NAME."
     fi
   fi
 fi
